baa552f194504bb67192637386678f0c5fcd535b
Jason Hostetter <jhostetter@gmail.com>
2012-11-10 14:33:29 -0500

    Test script page send working with static info

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var cheerio = require('cheerio'),
     tidy = require('htmltidy').tidy;
     
 var file = "!1a90f1cfhumdm_im";
+var pagerList = {};
 
 request.post( 'http://amion.com/cgi-bin/ocs', {form: {Login: 'mercymed'}},
             function( error, response, body ){
@@ -15,33 +16,59 @@ request.post( 'http://amion.com/cgi-bin/ocs', {form: {Login: 'mercymed'}},
                 
             });
 function getGroups() {
-request.get( 'https://www.amion.com/cgi-bin/ocs?File='+ file +'&Syr=2012&Page=Pgrsel&Rsel=-1',
-            function( error, response, body ){
-                //console.log(body);
-                
-                tidy(body, function(err,html){
-                                    
-                    var $ = cheerio.load(html);
-                    
-                    var selects = [];
-                    
-                    for (var i = 1; i < 10; i++) {
-                        selects.push($('[name="Rsel'+i+'"]'));
-                    }
-                                        
-                    var groups = {};
-                                        
-                    // loop through selects, get names and numbers
-                    $.each( selects, function(index, select){
-                        var options = select.find('option');
-                        var group = options.map( function(index,el){ var s = $(el).attr('value').split('*'); return {url:s[0],num:s[1],name:s[2]}; } );
-                        var label = options.label;
-                        groups[label] = group;
-                    });
-                    
-                    console.log(groups);
-                    
-                });
-                
-            });
+	request.get( 'https://www.amion.com/cgi-bin/ocs?File='+ file +'&Syr=2012&Page=Pgrsel&Rsel=-1',
+		function( error, response, body ){
+			//console.log(body);
+			
+			tidy(body, function(err,html){
+								
+				var $ = cheerio.load(html);
+				
+				var selects = [];
+				
+				for (var i = 1; i < 10; i++) {
+					selects.push($('[name="Rsel'+i+'"]').find('option'));
+				}
+														
+				// loop through selects, get names and numbers
+				$(selects).each( function(index, options){
+					var label = '';
+					options.each( function(index, option){
+						if (index === 0) {
+							label = $(option).text();
+							pagerList[label] = [];
+						} else {
+							var s = $(option).attr('value').split('*');
+							pagerList[label].push({url:s[0],num:s[1],name:s[2]});
+						}
+					});
+				});
+									
+				//console.log(pagerList);
+				
+				pageSomeone();
+				
+			});
+			
+		});
+}
+
+function pageSomeone(){
+	request.get({
+					url: 'https://www.amion.com/cgi-bin/ocs',
+					qs: { 
+						File: file,
+						Page: 'Alphapg', 
+						Rsel: '5', 
+						Syr: '2012',
+						Apgref: '1',
+						From: 'Jasontest',
+						Enote: 'Hey buddy'
+					}
+				},
+				function( error, response, body ){
+					console.log(response.statusCode);
+					console.log(body);
+				}
+	);
 }
