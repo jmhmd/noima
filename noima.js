f712dd80121fd8662134a30c417d8cd2b09426db
Jason Hostetter <jhostetter@gmail.com>
2012-11-18 17:38:01 -0500

    Function call chain now using express next() queue

--- noima.js
+++ noima.js
@@ -27,15 +27,185 @@ var nameList = [];
 
 
 //-----------Functions----------------//
-function refreshFile(req, callback){
+function refreshFile(req, res, next){
 	request.post( 'http://amion.com/cgi-bin/ocs', {form: {Login: 'mercymed'}},
 		function( error, response, body ){
-			var $ = cheerio.load(body);
-				
-			// set file, I think this is like a session cookie?
-			req.session.file = $('input[name="File"]').attr('value');
 			
-			callback();
+			tidy(body, function(err,html){
+				
+				var html = html.replace(/(\r\n|\n|\r)/gm," "); // remove carriage returns, line endings
+				
+				var $ = cheerio.load(html);
+				
+				// set file, I think this is like a session cookie?
+				req.session.file = $('input[name="File"]').attr('value');
+				req.landingHTML = $;
+				
+				next();
+			
+			});
+		});
+}
+
+function getOnCall(req, res, next) {
+// req.date format DD/MM/YY
+	
+	var onCallTeams,
+		onCall = [],
+		date = (typeof req.date !== 'undefined') ? req.date : 'today',
+		$;
+		
+	date = '18/11/12';
+			
+	if( date === 'today' && typeof req.landingHTML !== 'undefined' ) {
+		$ = req.landingHTML;
+		extractOnCall();
+	} else if (date !== 'today' && typeof date !== 'undefined') {
+		var s = date.split('/');
+		var month = s[1] + '-' + s[2];
+		var day = s[0];
+		
+		request.get( 'https://www.amion.com/cgi-bin/ocs?Month='+month+'&Day='+day+'&File='+req.session.file+'&Page=OnCall', function( error, response, body ){
+			if ( error ) {
+				throw new Error(error);
+			}
+			
+			tidy(body, function(err,html){
+			
+				html = html.replace(/(\r\n|\n|\r)/gm," "); // remove carriage returns, line endings
+				
+				$ = cheerio.load(html);
+				
+				req.session.file = $('input[name="File"]').attr('value');
+				
+				extractOnCall();
+			});
+		});
+	} else {
+		throw new Error('No day defined');
+	}
+	
+	function extractOnCall() {
+		// get rows from table
+		var rows = $('table').eq(1).find('tr');
+		// shift off header row
+		[].shift.call(rows);
+							
+		rows.each(function(index, row) {
+			var person = {};
+			person.name = $(row).find('a.plain').find('nobr').text().trim();
+			person.service = $(row).find('td').eq(0).text().replace(/\s+/g, " "); // remove nbsp;
+			person.training = $(row).find('td').eq(4).text();
+			var contactTD = $(row).find('td').eq(5);
+			person.contact = contactTD.find('a').length !== 0 ? {
+					number: contactTD.find('a').text().trim(),
+					link: contactTD.find('a').attr('href')
+				} : {
+					number: contactTD.find('nobr').text(),
+					link: false
+				};
+			onCall.push(person);
+		});
+		
+		function assignService(person, team) {
+			if( person.service.indexOf('Med '+team) > -1 ||
+				person.service.indexOf('Resident '+team) > -1 ) {
+				return true;
+			};
+		};
+		
+		// group onCall by service
+		onCallTeams = {
+				MAO: {
+					name: "MAO",
+					people: onCall.filter(function(person){ if(person.service.indexOf('MAO') !== -1) return true; })
+				},
+				medA: { 
+					name: "Med A",
+					people: onCall.filter(function(person){ return assignService(person,'A'); })
+				},
+				medB: {
+					name: "Med B",
+					people: onCall.filter(function(person){ return assignService(person,'B'); })
+				},
+				medC: {
+					name: "Med C",
+					people: onCall.filter(function(person){ return assignService(person,'C'); })
+				},
+				medY: {
+					name: "Med Y",
+					people: onCall.filter(function(person){ return assignService(person,'Y'); })
+				},
+				medX: {
+					name: "Med X",
+					people: onCall.filter(function(person){ return assignService(person,'X'); })
+				},
+				dayFloat: {
+					name: "Day Float",
+					people: onCall.filter(function(person){ if(person.service.indexOf('Day Float') !== -1) return true; })
+				},
+				nightFloat: {
+					name: "Night Float",
+					people: onCall.filter(function(person){ if(person.service.indexOf('NF Intern') !== -1 || person.service.indexOf('Overnight') !== -1) return true; })
+				},
+				hospitalist: {
+					name: "Daytime Hospitalist",
+					people: onCall.filter(function(person){ if(person.service.indexOf('Hospitalist') !== -1) return true; })
+				}
+			};
+			
+		req.onCall = onCall;
+		req.onCallTeams = onCallTeams;
+				
+		next();
+	}
+}
+
+function getPagerList(req, res, next){
+// TODO: figure out if Syr needs to change with calendar or academic year
+// can eventually skip this step for long running server as long as year is the same
+
+	var pagerList = {},
+		nameList = [];
+	
+	request.get( 'https://www.amion.com/cgi-bin/ocs?File='+ req.session.file +'&Syr=2012&Page=Pgrsel&Rsel=-1',
+		function( error, response, body ){
+			//console.log(body);
+			
+			tidy(body, function(err,html){
+								
+				var $ = cheerio.load(html);
+				
+				var selects = [];
+				
+				for (var i = 1; i < 10; i++) {
+					selects.push($('[name="Rsel'+i+'"]').find('option'));
+				}
+														
+				// loop through selects, get names and numbers
+				$(selects).each( function(index, options){
+					var label = '';
+					options.each( function(index, option){
+						if (index === 0) {
+							label = $(option).text();
+							pagerList[label] = [];
+						} else {
+							var s = $(option).attr('value').split('*');
+							pagerList[label].push({url:s[0],num:s[1],name:s[2]});
+							if ( typeof s[2] !== 'undefined' ) {
+								nameList.push( s[2] );
+							}
+						}
+					});
+				});
+				
+				req.pagerList = pagerList;
+				req.nameList = nameList;
+									
+				next();
+				
+			});
+			
 		});
 }
 
@@ -92,163 +262,17 @@ app.post('/sendPage', function(req, res){
 	sendPage();
 });
 
-app.get('/', function(req, res){
+app.get('/onCall', function(req,res) {
 	
-	var onCallTeams;
-    
-    function getOnCall(){
-		
-		onCall = []; // clear list if any loaded already
-		req.session.file = "";
-		
-        request.post( 'http://amion.com/cgi-bin/ocs', {form: {Login: 'mercymed'}},
-            function( error, response, body ){
-                //console.log(body);
-                
-                tidy(body, function(err,html){
-				
-					html = html.replace(/(\r\n|\n|\r)/gm," "); // remove carriage returns, line endings
-                    
-                    var $ = cheerio.load(html);
-				
-					// set file, I think this is like a session cookie?
-					req.session.file = $('input[name="File"]').attr('value');
-					
-					// get rows from table
-					var rows = $('table').eq(1).find('tr');
-					// shift off header row
-					[].shift.call(rows);
-										
-					rows.each(function(index, row) {
-						var person = {};
-						person.name = $(row).find('a.plain').find('nobr').text().trim();
-						person.service = $(row).find('td').eq(0).text().replace(/\s+/g, " "); // remove nbsp;
-						person.training = $(row).find('td').eq(4).text();
-						var contactTD = $(row).find('td').eq(5);
-						person.contact = contactTD.find('a').length !== 0 ? {
-								number: contactTD.find('a').text().trim(),
-								link: contactTD.find('a').attr('href')
-							} : {
-								number: contactTD.find('nobr').text(),
-								link: false
-							};
-						onCall.push(person);
-					});
-					
-					function assignService(person, team) {
-						if( person.service.indexOf('Med '+team) > -1 ||
-							person.service.indexOf('Resident '+team) > -1 ) {
-							return true;
-						};
-					};
-					
-					// group onCall by service
-					onCallTeams = {
-							MAO: {
-								name: "MAO",
-								people: onCall.filter(function(person){ if(person.service.indexOf('MAO') !== -1) return true; })
-							},
-							medA: { 
-								name: "Med A",
-								people: onCall.filter(function(person){ return assignService(person,'A'); })
-							},
-							medB: {
-								name: "Med B",
-								people: onCall.filter(function(person){ return assignService(person,'B'); })
-							},
-							medC: {
-								name: "Med C",
-								people: onCall.filter(function(person){ return assignService(person,'C'); })
-							},
-							medY: {
-								name: "Med Y",
-								people: onCall.filter(function(person){ return assignService(person,'Y'); })
-							},
-							medX: {
-								name: "Med X",
-								people: onCall.filter(function(person){ return assignService(person,'X'); })
-							},
-							dayFloat: {
-								name: "Day Float",
-								people: onCall.filter(function(person){ if(person.service.indexOf('Day Float') !== -1) return true; })
-							},
-							nightFloat: {
-								name: "Night Float",
-								people: onCall.filter(function(person){ if(person.service.indexOf('NF Intern') !== -1 || person.service.indexOf('Overnight') !== -1) return true; })
-							},
-							hospitalist: {
-								name: "Daytime Hospitalist",
-								people: onCall.filter(function(person){ if(person.service.indexOf('Hospitalist') !== -1) return true; })
-							}
-						};
-					
-					getPagerList();
-                    
-                    //res.render('index', {names: names, service: service, training: training, contact: contact});
-                
-                });
-                
-            });
-    }
-    
-    function getPagerList(){
-    // TODO: figure out if Syr needs to change with calendar or academic year
-	// can eventually skip this step for long running server as long as year is the same
-    
-		pagerList = {}; // clear list if any loaded
-		nameList = [];
-		
-        request.get( 'https://www.amion.com/cgi-bin/ocs?File='+ req.session.file +'&Syr=2012&Page=Pgrsel&Rsel=-1',
-			function( error, response, body ){
-				//console.log(body);
-				
-				tidy(body, function(err,html){
-									
-					var $ = cheerio.load(html);
-					
-					var selects = [];
-					
-					for (var i = 1; i < 10; i++) {
-						selects.push($('[name="Rsel'+i+'"]').find('option'));
-					}
-															
-					// loop through selects, get names and numbers
-					$(selects).each( function(index, options){
-						var label = '';
-						options.each( function(index, option){
-							if (index === 0) {
-								label = $(option).text();
-								pagerList[label] = [];
-							} else {
-								var s = $(option).attr('value').split('*');
-								pagerList[label].push({url:s[0],num:s[1],name:s[2]});
-								if ( typeof s[2] !== 'undefined' ) {
-									nameList.push( s[2] );
-								}
-							}
-						});
-					});
-										
-					renderPage();
-					
-				});
-				
-			});
-    }
-    
-    function renderPage() {
-		console.log(onCall);
-		//console.log(pagerList);
-		//console.log(onCallTeams);
-		
-		res.render('index', { onCall: onCall, onCallTeams: onCallTeams, pagerList: pagerList, nameList: nameList });
-	}
+});
+
+app.get('/', refreshFile, getOnCall, getPagerList, function(req, res) {
 	
-	function init() {
-		getOnCall();
-	}
+	//console.log(pagerList);
+	//console.log(onCallTeams);
 	
-	init();
+	res.render('index', { onCall: req.onCall, onCallTeams: req.onCallTeams, pagerList: req.pagerList, nameList: req.nameList });
+
 });
 
 
